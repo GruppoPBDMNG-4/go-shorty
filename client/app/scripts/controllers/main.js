fca28142c9ae6f37c1b0438d8788c1a89261c207
paolobinni <paolo.saracino.ps@gmail.com>
2015-09-04 18:36:23 +0200

    second angular

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -13,13 +13,13 @@ angular.module('clientApp')
   .controller('MainCtrl', function ($scope,$http) {
     $scope.longUrl = "here";
     $scope.custom = "here";
-    $scope.btn = "GO!"
+    $scope.btn = "GO!";
   	$scope.takeShort = function() {
   		$http.post("/rest/go-shorty",{longUrl : $scope.longUrl,custom:$scope.custom})
   		.success(function(data) {
   			console.log('Inserted!');
   			$scope.rispostaJson = data;
   			$scope.shortUrl = "http://localhost:4567/"+$scope.rispostaJson.shortUrl;
-  			
+
   		});};
   });
